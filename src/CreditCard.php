9c0d37392401187bd33dc1fe7c7aec9e3b817097
Brendan Bullen <bullenb@gmail.com>
2021-08-18 15:01:39 +0100

    Add card type

--- src/CreditCard.php
+++ src/CreditCard.php
@@ -28,4 +28,33 @@ class CreditCard extends BaseCreditCard
             throw new InvalidCreditCardException('Card has expired');
         }
     }
+
+    /**
+     * @return string|null
+     */
+    public function getBrand(): ?string
+    {
+        return $this->getCardType() ?? parent::getBrand();
+    }
+
+    /**
+     * Get Card type.
+     *
+     * @return string|null
+     */
+    public function getCardType(): ?string
+    {
+        return $this->getParameter('cardType');
+    }
+
+    /**
+     * Set Card type.
+     *
+     * @param string|null $value Parameter value
+     * @return $this
+     */
+    public function setCardType(?string $value): CreditCard
+    {
+        return $this->setParameter('cardType', $value);
+    }
 }
